d8c9f89401f2c158b81cf692b7bb5016a5b73a7d
s26083 <s26083@pjwstk.edu.pl>
2023-03-16 21:01:49 +0100

    Added solution to task requirement 4 - open 3 archived pages.

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import webbrowser
-if __name__ == '__main__':
-    print("hello world")
-    url = input("Podaj stronę internetową: ")
-    webbrowser.open(url)
+import requests as requests
+date = "20230315"
+pageurl = input("Podaj adres strony internetowej:")
+for x in range(3):
+    url = "http://archive.org/wayback/available?url="+pageurl+"&timestamp="+str(date)
+    response = requests.get(url)
+    d = response.json()
+    page = d["archived_snapshots"]["closest"]["url"]
+    webbrowser.open(page)
+    date = int(date)
+    date = date-100000
+    date = str(date)
